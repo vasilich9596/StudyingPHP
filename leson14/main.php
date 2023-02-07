97c4deb473bd3f8dd874fe7b03fd44a49e63e07d
vasilich <vasilich68@gmail.com>
2023-02-07 13:04:38 +0200

    lesson-14-csv file rw

--- leson14/main.php
+++ leson14/main.php
@@ -5,7 +5,7 @@ date_default_timezone_set('UTC');
 ini_set('display_errors', 1);
 ini_set('error_reporting', E_ALL);
 
-$file = fopen(__DIR__ . '/lesson-14-report.xlsx - Sheet.csv', 'c+');
+$file = fopen(__DIR__ . '/lesson-14-report.xlsx - Sheet.csv', 'r+');
 
 $reports = [];
 
@@ -16,49 +16,58 @@ while (!feof($file)) {
 
     $reports[] = [
         'mode' => $row[1],
-        'date' => $row[2],
+        'date' => date_create_from_format('Y-m-d H:i:s', $row[2]),
         'type' => $row[4],
-        'amount' => $row[5]
+        'amount' => (int)$row[5]
     ];
 
 }
+$cardTotal = [];
+$cashTotal = [];
+$return = [];
+$outputReport = [];
 
+foreach ($reports as $report) {
 
-$totalReports = [];
-$cardTotal = 0;
-$cashTotal = 0;
+    $date = date_format($report['date'], 'Y-m-d');
 
-foreach ($reports as $data) {
-    $mode = $data['mode'];
-    $type = $data['type'];
-    $date = $data['date'];
-    $amount = $data['amount'];
-    if (!array_key_exists($date, $totalReports)) {
-        $totalReports[$date] = [];
+    if (!array_key_exists($date, $outputReport)) {
+        $outputReport[$date] = [
+            'cash' => 0,
+            'card' => 0,
+            'return' => 0,
+        ];
     }
-
-    if (!array_key_exists($mode, $totalReports[$date])) {
-        $totalReports[$date][$mode] = [];
+    if ($date == true) {
+        if ($report['mode'] == 'SELL') {
+            if ($report['type'] == 'Готівка') {
+                $outputReport[$date]['cash'] += $report['amount'];
+            }
+        }
     }
-
-    if (!array_key_exists($type, $totalReports[$date][$mode])) {
-        $totalReports[$date][$mode][$type] = [];
+    if ($date == true) {
+        if ($report['mode'] == 'SELL') {
+            if ($report['type'] == 'Картка') {
+                $outputReport[$date]['card'] += $report['amount'];
+            }
+        }
     }
-
-        $totalReports[$date][$mode][$type] = $amount;
+    if ($date == true) {
+        if ($report['mode'] == 'RETURN') {
+            $outputReport[$date]['return'] += $report['amount'];
+        }
+    }
 
 }
-print_r($totalReports);
 
 
-//    if ($totalReports[$date][$mode] != 'RETURN') {
-//        if ($totalReports[$date][$mode][$type] == 'Картка') {
-//            $cardTotal = count($data['amount']);
-//        } elseif ($totalReports[$mode] == 'Готівка') {
-//            $cashTotal = count($data['amount']);
-//        }
-//    } else {
-//        $return = count($data['amount']);
-//    }
-
-
+$fileWrite = fopen(__DIR__ . '/lesson-14-Sheet.csv', 'w+');
+ksort($outputReport);
+foreach ($outputReport as $date => $info) {
+    fputcsv($fileWrite, [
+        $date,
+        $info['cash'],
+        $info['card'],
+        $info['return']
+    ]);
+}
